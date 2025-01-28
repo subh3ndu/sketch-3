3ba61cbf8fde9be27196386059a153c510ff8bf2
subh3ndu <subhendukarmakar61@gmail.com>
2025-01-28 13:11:59 +0000

    minor improvements II

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let ballA, ballB
+let ballA, ballB, ballC, ballD
 
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  ballA = new Ball(width/2 - 200, height/2, 2)
-  ballB = new Ball(width/2 + 200, height/2, 4)
-
+  ballA = new Ball(width/2 - 200, height/2, 1)
+  ballB = new Ball(width/2 - 100, height/2, 2)
+  ballC = new Ball(width/2,       height/2, 4)
+  ballD = new Ball(width/2 + 100, height/2, 8)
+  ballE = new Ball(width/2 + 200, height/2, 16)
 }
 
 function draw() {
@@ -14,6 +16,9 @@ function draw() {
     let wind = createVector(0.1, 0)
     ballA.applyForce(wind)
     ballB.applyForce(wind)
+    ballC.applyForce(wind)
+    ballD.applyForce(wind)
+    ballE.applyForce(wind)
   }
 
 
@@ -21,15 +26,34 @@ function draw() {
 
   let weightA = p5.Vector.mult(gravity, ballA.mass) ///  NOTE: force(weight) due to mass
   let weightB = p5.Vector.mult(gravity, ballB.mass)
+  let weightC = p5.Vector.mult(gravity, ballC.mass) ///  NOTE: force(weight) due to mass
+  let weightD = p5.Vector.mult(gravity, ballD.mass)
+  let weightE = p5.Vector.mult(gravity, ballE.mass)
   ballA.applyForce(weightA)
   ballB.applyForce(weightB)
+  ballC.applyForce(weightC)
+  ballD.applyForce(weightD)
+  ballE.applyForce(weightE)
 
   ballA.update()
   ballA.edges()
   ballA.show()
+
   ballB.update()
   ballB.edges()
   ballB.show()
+
+  ballC.update()
+  ballC.edges()
+  ballC.show()
+
+  ballD.update()
+  ballD.edges()
+  ballD.show()
+
+  ballE.update()
+  ballE.edges()
+  ballE.show()
 
   // text config
   textFont('Arial')
